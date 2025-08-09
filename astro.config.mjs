6042c65795ba4678425dadd4538f64c41b394e5b
joaquin <joaquinsilvagvg@gmail.com>
2025-08-08 22:04:30 -0400

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://JqSilva.github.io/landing',
-  base: '/landing/',
+  site: 'https://JqSilva.github.io',
+  base: 'landing-copy',
   vite: {
     plugins: [tailwindcss()]
   },
