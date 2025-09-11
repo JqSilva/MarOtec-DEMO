e7f4dea83b2d6b417876792ad73fa7780b940b93
Joaquin <joaquinsilvagvg@gmail.com>
2025-09-11 11:15:35 -0300

    Configuracion para Hostinger - Version Produccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://JqSilva.github.io',
-  base: 'MarOtec-DEMO',
+  site: 'https://mar-otec.com',
   vite: {
     plugins: [tailwindcss()]
   },
